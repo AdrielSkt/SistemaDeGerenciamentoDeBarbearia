c56b226475dff326eb9c3fef09f57e06ef0be645
Caio Reis <caiomagalhaesreis@hotmail.com>
2023-02-06 22:28:10 -0300

    erro de body

--- frontWeb/src/app/components/servicos/servicos.module.ts
+++ frontWeb/src/app/components/servicos/servicos.module.ts
@@ -5,13 +5,40 @@ import { ServicosReadComponent } from './servicos-read/servicos-read.component';
 
 
 
+
+
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { RouterModule } from '@angular/router';
+
+
 @NgModule({
   declarations: [
     ServicosReadComponent
   ],
   imports: [
     CommonModule,
-    ServicosRoutingModule
+    ServicosRoutingModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule,
+    MatProgressBarModule,
+    RouterModule,
+    ReactiveFormsModule,
+
   ]
 })
 export class ServicosModule { }
