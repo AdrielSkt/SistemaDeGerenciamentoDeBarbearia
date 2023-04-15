efd6f6f20c6a3384c6bb06977de16bb0882bbe9e
osorio.gomes <osorio.gomes@castgroup.com.br>
2023-04-15 11:27:15 -0300

    mobile finalizando footerBar

---
+++ BarberApp/src/components/ButtonAgendar/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {Entypo, Feather} from '@expo/vector-icons';
+export default function ButtonAgendar ({size,focused}){
+
+  return(
+    <View style={[styles.container, {backgroundColor: focused ? '#0B2BCA' : '#FFFFFF'}]}>
+      <Feather name="scissors" color={focused ? '#FFFFFF':'#121212'} size={size}/>
+    </View>
+  )
+
+}
+
+
+const styles = StyleSheet.create({
+  container:{
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#0B2BCA',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  }
+})
--- BarberApp/src/pages/routes.js
+++ BarberApp/src/pages/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet,} from "react-native";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-
+import {Entypo, Feather} from '@expo/vector-icons';
+import ButtonAgendar from "../components/ButtonAgendar";
 import Menu from './Menu';
 import Agendar from './Agendar';
 import Agendamentos from './Agendamentos';
@@ -13,13 +13,19 @@ const Tab = createBottomTabNavigator();
 export default function Routes(){
   return(
 
-    <Tab.Navigator screenOptions={{ headerShown: false}} >
-      <Tab.Screen name="Menu" component={Menu}/>
-      <Tab.Screen name="Agendar" component={Agendar}/>
-      <Tab.Screen name="Agendamentos" component={Agendamentos}/>
-      <Tab.Screen name="Perfil" component={Perfil}/>
-      <Tab.Screen name="Sobre" component={Sobre}/>
+    <Tab.Navigator screenOptions={{ headerShown: false, 
+    tabBarStyle: { 
+      position: 'absolute', 
+      backgroundColor: 'black' },
+      tabBarActiveTintColor: '#0B2BCA',
+      }} >
+      <Tab.Screen name="Menu" component={Menu} options={{tabBarIcon: ({color, size})=> (<Entypo name="documents" color={color} size={size}/>)}}/>
+      <Tab.Screen name="Agendamentos" component={Agendamentos} options={{tabBarIcon: ({color, size})=> (<Entypo name="colours" color={color} size={size}/>)}}/>
+      <Tab.Screen name="Agendar" component={Agendar} options={{tabBarLabel: '', tabBarIcon: ({ size, focused})=> (<ButtonAgendar size={size} focused={focused}/>)}}/>
+      <Tab.Screen name="Perfil" component={Perfil} options={{tabBarIcon: ({color, size})=> (<Entypo name="v-card" color={color} size={size}/>)}}/>
+      <Tab.Screen name="Sobre" component={Sobre} options={{tabBarIcon: ({color, size})=> (<Entypo name="info-with-circle" color={color} size={size}/>)}}/>
     </Tab.Navigator>
 
   )
 }
+{/* <Feather name="scissors" color={color} size={size}/> */}
